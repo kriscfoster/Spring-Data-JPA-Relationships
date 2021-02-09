52d668bbb1c31c369d26dafc45a6f4c7a5bd20e9
kriscfoster <krisfoster@live.ie>
2021-02-09 20:09:39 +0000

    initial commit

---
+++ src/main/java/com/kriscfoster/school/SchoolApplication.java
@@ -0,0 +1,13 @@
+package com.kriscfoster.school;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SchoolApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SchoolApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/kriscfoster/school/SchoolApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kriscfoster.school;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SchoolApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
